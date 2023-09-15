9b7d90c736444f9d090c0ddde7e82233f07138d4
Meg Walker <meg.walker56@gmail.com>
2023-09-15 19:54:03 +0100

    started building content installed react router

--- reactportfolio/src/App.jsx
+++ reactportfolio/src/App.jsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Home</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Projects</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<section>
+  <div className ="jumbotron">
+    <h1>Megan Walker-Lange</h1>
+    <p>Hi! Welcome to my portfolio page. Here you will be able to find out about my latest projects and work. I create apps and love to design things to look beautiful. If you would like to work with me please feel free to get in touch byt checking out the contact page. Links to my gutHub and LinkedIn can be found further down the page. </p>
+  </div>
+</section>
+
+  
     </>
   )
 }
---
+++ reactportfolio/src/components/contact.jsx
@@ -0,0 +1,17 @@
+ import '../App.jsx'
+import '../App.css'
+
+
+function contact() {
+
+    return (
+<>
+<section>
+    <p>Please find my contact details and professional pages below</p>
+    <link href='https://github.com/megwalkerlange'>GitHub Page</link>
+</section>
+</>)
+    }
+
+
+export default contact
